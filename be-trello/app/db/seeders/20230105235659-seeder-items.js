0e965e23ee6e488e24c039a76e81d02cc46110aa
jehianth <jehianbusiness.com>
2023-01-06 07:17:03 +0700

    run seed

--- be-trello/app/db/seeders/20230105235659-seeder-items.js
+++ be-trello/app/db/seeders/20230105235659-seeder-items.js
@@ -3,23 +3,23 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Items', [
+      {
+        name: 'New website codeathome',
+        TodoId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'UIUX',
+        TodoId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Items', null, {});
   }
 };
