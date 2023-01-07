f0724e8991fc53abfdf608a8679171693e2c2016
jehianth <jehianbusiness.com>
2023-01-07 07:47:10 +0700

    7/1/23

--- be-trello/app/db/models/item.js
+++ be-trello/app/db/models/item.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Item.belongsTo(models.Todo);
     }
   }
   Item.init({
