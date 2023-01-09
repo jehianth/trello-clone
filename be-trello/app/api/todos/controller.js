79112bda5018d70a984b63533c60fce9b97df259
jehianth <jehianbusiness.com>
2023-01-09 13:02:45 +0700

    include attribute

--- be-trello/app/api/todos/controller.js
+++ be-trello/app/api/todos/controller.js
@@ -1,10 +1,15 @@
-const {Todo} = require('../../db/models');
+const {Todo, Item} = require('../../db/models');
 // client req endpoint todo, lalu respon yang dibalikin client apa
 module.exports = {
     getAll : async(req, res) => {
         try {
             const result = await Todo.findAll({
-                attributes: ['id', 'name']
+                attributes: ['id', 'name'],
+                //incluce = memasukan relasi   
+                include: {
+                    model: Item,
+                    attributes: ['id','name','TodoID']
+                },
             });
             res.status(200).json({
                 message: 'success',
